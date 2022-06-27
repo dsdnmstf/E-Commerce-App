4bb56a4fac57e7c2d2ba8fc4127b67dad4581ede
musrafa <dsdnmstf@gmail.com>
2022-06-27 21:16:40 +0200

    card responsive added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,172 +1,168 @@
-import { Add, Remove } from "@material-ui/icons"
-import styled from "styled-components"
-import Announcment from "../components/Announcment"
-import Footer from "../components/Footer"
-import Navbar from "../components/Navbar"
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Annoucement";
+import Footer from "../components/Footer";
+import { Add, Remove } from "@material-ui/icons";
+import mobile from "../responsive";
 
-const Container = styled.div``
-
+const Container = styled.div``;
 const Wrapper = styled.div`
-padding: 20px;`;
+  padding: 20px;
+  ${mobile({ padding: "10px" })}
+`;
 const Title = styled.h1`
-font-weight: 300;
-text-align:center;
+  font-weight: 300;
+  text-align: center;
 `;
-
 const Top = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-padding: 20px;
-`
-
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
 const TopButton = styled.button`
-padding: 10px;
-font-weight: 600;
-cursor:pointer;
-border: ${(props) => props.type === "filled" && "none"};
-background-color: ${(props) => props.type === "filled" ? "black" : "transparent"};
-color: ${(props) => props.type === "filled" && "white"};
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparant"};
+  color: ${(props) => props.type === "filled" && "white"};
 `;
 
-
-const TopTexts = styled.div``
-
-const TopText=styled.span`
-text-decoration:underline;
-cursor:pointer;
-margin:0 10px;
-`
-
+const TopTexts = styled.div`
+  ${mobile({ display: "none" })}
+`;
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+`;
 const Bottom = styled.div`
-display: flex;
-justify-content:space-between;
-
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
-
 const Info = styled.div`
-flex:3;
+  flex: 3;
 `;
 
 const Product = styled.div`
-display: flex;
-justify-content:space-between;
-`
-const ProductDetail = styled.div`
-flex:2;
-display: flex;
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
-
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
 const Image = styled.img`
-width: 200px;`;
-
+  width: 200px;
+`;
 const Details = styled.div`
-padding: 20px;
-display: flex;
-flex-direction:column;
-justify-content:space-around;
-
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 `;
 const ProductName = styled.span``;
-
 const ProductId = styled.span``;
-
 const ProductColor = styled.div`
-width: 20px;
-height: 20px;
-border-radius:50%;
-background-color: ${props=>props.color};
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
 `;
 const ProductSize = styled.span``;
-
 const PriceDetail = styled.div`
   flex: 1;
+
   display: flex;
-  flex-direction:column;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  justify-content:center;
 `;
 
-const ProductAmountContainer=styled.div`
-display: flex;
-align-items: center;
-margin-bottom: 20px;
-`
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 const ProductAmount = styled.div`
-font-size: 24px;
-margin: 5px;`;
-
+  font-size: 24px;
+  margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
+`;
 const ProductPrice = styled.div`
-font-size: 30px;
-font-weight: 200;
+  font-size: 30px;
+  font-weight: 200;
+  ${mobile({ marginBottom: "20px" })}
 `;
 
 const Hr = styled.hr`
-background-color:#eee;
-border:none;
-height:1px;
+  background-color: #eee;
+  border: none;
+  height: 1px;
 `;
 
 const Summary = styled.div`
   flex: 1;
-  border: 0.5px solid black;
-  border-radius:10px;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
   padding: 20px;
   height: 50vh;
 `;
-
 const SummaryTitle = styled.h1`
-font-weight: 200;
-`
+  font-weight: 200;
+`;
 const SummaryItem = styled.div`
   margin: 30px 0px;
   display: flex;
   justify-content: space-between;
-  font-weight: ${(props) => props.type == "total" && 500};
-  font-size: ${(props) => props.type == "total" && "24px"};
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
 `;
 const SummaryItemText = styled.span``;
-
 const SummaryItemPrice = styled.span``;
-
 const Button = styled.button`
-width: 100px;
-padding: 10px;
-background-color:black;
-color:white;
-font-weight: 600;
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
 `;
 
 const Cart = () => {
   return (
     <Container>
       <Navbar />
-      <Announcment />
+      <Announcement />
       <Wrapper>
-        <Title>Your Bag</Title>
+        <Title> YOUR BAG</Title>
         <Top>
-          <TopButton>CONTINOU SHOPPING</TopButton>
+          <TopButton> CONTINUE SHOPPING</TopButton>
           <TopTexts>
-            <TopText>Shopping Bag(2)</TopText>
-            <TopText>Your Wish List(1)</TopText>
+            <TopText> Shopping Bag(2)</TopText>
+            <TopText> Your Wishlist (0)</TopText>
           </TopTexts>
-          <TopButton type="filled">CHECKOUT NOW</TopButton>
+          <TopButton type="filled"> CHECKOUT NOW</TopButton>
         </Top>
         <Bottom>
           <Info>
             <Product>
               <ProductDetail>
-                <Image src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?b=1&k=20&m=1308274455&s=170667a&w=0&h=S3UYmEJ1hUri2rB8jRhJ4wNY31lMrHysLHbl9YD8j_Q=" />
+                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
                 <Details>
                   <ProductName>
-                    <b>Product:</b> JESSIE THUNDER SHOESS
+                    <b>Product: </b> JESSIE THUNDER SHOES
                   </ProductName>
                   <ProductId>
-                    <b>ID:</b> 938564521
+                    <b>ID: </b> 934354567567
                   </ProductId>
                   <ProductColor color="black" />
+
                   <ProductSize>
-                    <b>Size:</b> 37.5
+                    <b>Size: </b> 37.5
                   </ProductSize>
                 </Details>
               </ProductDetail>
@@ -182,17 +178,18 @@ const Cart = () => {
             <Hr />
             <Product>
               <ProductDetail>
-                <Image src="https://media.istockphoto.com/photos/blue-sneakers-isolated-on-white-background-picture-id1308274455?b=1&k=20&m=1308274455&s=170667a&w=0&h=S3UYmEJ1hUri2rB8jRhJ4wNY31lMrHysLHbl9YD8j_Q=" />
+                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
                 <Details>
                   <ProductName>
-                    <b>Product:</b> JESSIE THUNDER SHOES
+                    <b>Product: </b> HALO T-SHIRT
                   </ProductName>
                   <ProductId>
-                    <b>ID:</b> 938564521
+                    <b>ID: </b> 65787987956
                   </ProductId>
-                  <ProductColor color="black" />
+                  <ProductColor color="gray" />
+
                   <ProductSize>
-                    <b>Size:</b> 37.5
+                    <b>Size: </b> XL
                   </ProductSize>
                 </Details>
               </ProductDetail>
@@ -202,35 +199,35 @@ const Cart = () => {
                   <ProductAmount>2</ProductAmount>
                   <Remove />
                 </ProductAmountContainer>
-                <ProductPrice>$ 30</ProductPrice>
+                <ProductPrice>$ 20</ProductPrice>
               </PriceDetail>
             </Product>
           </Info>
           <Summary>
             <SummaryTitle>ORDER SUMMARY</SummaryTitle>
             <SummaryItem>
-              <SummaryItemText>Subtotal</SummaryItemText>
-              <SummaryItemText>$ 80</SummaryItemText>
+              <SummaryItemText>SubTotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem>
-              <SummaryItemText>Estimated Shipping</SummaryItemText>
-              <SummaryItemText>$ 5.90</SummaryItemText>
+              <SummaryItemText>Estimated Shipping </SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
             </SummaryItem>
             <SummaryItem>
               <SummaryItemText>Shipping Discount</SummaryItemText>
-              <SummaryItemText>$ 5.90</SummaryItemText>
+              <SummaryItemPrice>$ -5.90 </SummaryItemPrice>
             </SummaryItem>
             <SummaryItem type="total">
               <SummaryItemText>Total</SummaryItemText>
-              <SummaryItemText>$ 80</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
             </SummaryItem>
-            <Button>CHECK OUT NOW</Button>
+            <Button>CHECKOUT NOW</Button>
           </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
     </Container>
   );
-}
+};
 
-export default Cart
+export default Cart;
