4d79ce5e4a492edecd13d56912292139be0dca16
musrafa <dsdnmstf@gmail.com>
2022-06-27 21:19:16 +0200

    Product list responsive added

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,35 +1,45 @@
 import styled from "styled-components";
-import Navbar from "../components/Navbar";
-import Announcment from "../components/Announcment";
-import Products from "../components/Products";
-import NewsLetter from "../components/NewsLetter";
+import Announcement from "../components/Annoucement";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import mobile from "../responsive";
+
 const Container = styled.div``;
+
 const Title = styled.h1`
   margin: 20px;
 `;
+
 const FilterContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
+
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ margin: "0px 20px", display: "flex", flexDirection: "column" })}
 `;
+
 const FilterText = styled.span`
-  font-style: 20px;
+  font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ margin: "0px" })}
 `;
+
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
 `;
 const Option = styled.option``;
 const ProductList = () => {
   return (
     <Container>
       <Navbar />
-      <Announcment />
+      <Announcement />
       <Title>Dresses</Title>
       <FilterContainer>
         <Filter>
@@ -54,20 +64,19 @@ const ProductList = () => {
             <Option>M</Option>
             <Option>L</Option>
             <Option>XL</Option>
-            <Option>XXL</Option>
           </Select>
         </Filter>
         <Filter>
           <FilterText>Sort Products:</FilterText>
           <Select>
             <Option selected>Newest</Option>
-            <Option>Prise (asc)</Option>
+            <Option>Price (asc)</Option>
             <Option>Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
       <Products />
-      <NewsLetter />
+      <Newsletter />
       <Footer />
     </Container>
   );
