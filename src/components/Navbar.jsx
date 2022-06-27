8c5c711ab668b9ca0776168f5eef2b1c5c1271ae
musrafa <dsdnmstf@gmail.com>
2022-06-27 21:13:43 +0200

    responsive.jsx added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
-const Container = styled.div``;
+const Container = styled.div`
+  ${mobile({ backgroundColor: "red" })}
+`;
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
