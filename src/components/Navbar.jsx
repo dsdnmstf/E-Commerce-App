29012a701fa9aff59e8d821bfab6e675afaa4026
musrafa <dsdnmstf@gmail.com>
2022-06-27 21:22:07 +0200

    navbar responsive added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,33 @@
 import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
 import styled from "styled-components";
-import { mobile } from "../responsive";
-
+import mobile from "../responsive";
 const Container = styled.div`
-  ${mobile({ backgroundColor: "red" })}
+  height: 60px;
+  ${mobile({ height: "50px" })}
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: space-between;
+  ${mobile({ paadding: "10px 0px" })}
 `;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
 `;
+
 const Language = styled.span`
-  font-style: 14px;
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
 const SearchContainer = styled.div`
-  border: 0.5px solid lightgray;
+  border: 1px solid lightgray;
   display: flex;
   align-items: center;
   margin-left: 25px;
@@ -29,48 +35,54 @@ const SearchContainer = styled.div`
 `;
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `;
+
 const Center = styled.div`
   flex: 1;
   text-align: center;
 `;
-
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
+
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
-const MenuItem = styled.div`
-  font-style: 14px;
+
+const MenuLink = styled.div`
+  font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
-          <Language>En</Language>
+          <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>HALO.</Logo>
         </Center>
         <Right>
-          <MenuItem>Register</MenuItem>
-          <MenuItem>Sign In</MenuItem>
-          <MenuItem>
+          <MenuLink>REGISTER</MenuLink>
+          <MenuLink>SIGN IN</MenuLink>
+          <MenuLink>
             <Badge badgeContent={4} color="primary">
               <ShoppingCartOutlined />
             </Badge>
-          </MenuItem>
+          </MenuLink>
         </Right>
       </Wrapper>
     </Container>
