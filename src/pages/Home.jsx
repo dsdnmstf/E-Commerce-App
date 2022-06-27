61d121f03e229ce1c75c2e3592ee4222707a1edf
musrafa <dsdnmstf@gmail.com>
2022-06-27 21:18:30 +0200

    product responsive added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import Announcment from "../components/Announcment";
+import Annoucement from "../components/Annoucement";
 import Categories from "../components/Categories";
 import Footer from "../components/Footer";
+
 import Navbar from "../components/Navbar";
-import NewsLetter from "../components/NewsLetter";
+import Newseletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
-      <Announcment />
+      <Annoucement />
       <Navbar />
       <Slider />
       <Categories />
       <Products />
-      <NewsLetter />
+      <Newseletter />
       <Footer />
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,71 +1,76 @@
 import styled from "styled-components";
+import mobile from "../responsive";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(
-      rgba(255, 255, 255, 0.3),
-      rgba(255, 255, 255, 0.3)
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
     ),
-    url("https://cdn.pixabay.com/photo/2021/11/25/13/05/woman-6823558__340.jpg");
-  background-size:cover;
+    url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 const Wrapper = styled.div`
-  padding: 20px;
   width: 25%;
+  padding: 20px;
   background-color: white;
+  ${mobile({ width: "75%" })}
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
 `;
 const Form = styled.form`
   display: flex;
-  flex-direction:column
+  flex-direction: column;
 `;
-
-const Title = styled.h1`
-  font-style: 24px;
-  font-weight: 300;
-`;
-
 const Input = styled.input`
   flex: 1;
   min-width: 40%;
-  margin: 10px 0;
   padding: 10px;
+  margin: 10px 0px;
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
 `;
 
 const Button = styled.button`
   width: 40%;
   border: none;
   padding: 15px 20px;
-  cursor: pointer;
   background-color: teal;
   color: white;
+  cursor: pointer;
   margin-bottom: 10px;
 `;
 
-const Link = styled.a`
-margin: 5px 0;
-font-style: 12px;
-text-decoration: underline;
-cursor:pointer;
-`
-const Register = () => {
+const Login = () => {
   return (
     <Container>
       <Wrapper>
-        <Title>SIGN IN</Title>
+        <Title> SIGN IN</Title>
+
         <Form>
           <Input placeholder="username" />
+
           <Input placeholder="password" />
-          <Button>LOGIN</Button>
-          <Link>Do Not You REMEMBER THE PASSWORD?</Link>
-          <Link>CREATE A NEW ACCOUNT</Link>
+
+          <Button>LOG IN</Button>
+          <Link> DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link> CREATE A NEW ACCOUNT</Link>
         </Form>
       </Wrapper>
     </Container>
   );
 };
 
-export default Register;
+export default Login;
